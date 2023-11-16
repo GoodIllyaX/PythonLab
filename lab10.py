488e573c2b103822968661c18bbddc0c9c75a730
Illya <67513471+GoodIllyaX@users.noreply.github.com>
2023-11-16 09:18:10 +0200

    Update lab10.py

--- lab10.py
+++ lab10.py
@@ -1,22 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Завантаження даних
-equipment_df = pd.read_csv('russia_losses_equipment.csv')  # обладнання
-corrections_df = pd.read_csv('russia_losses_equipment_correction.csv')  # техніка
-personnel_df = pd.read_csv('russia_losses_personnel.csv') # персональній состав
+equipment_df = pd.read_csv('russia_losses_equipment.csv') 
+corrections_df = pd.read_csv('russia_losses_equipment_correction.csv') 
+personnel_df = pd.read_csv('russia_losses_personnel.csv') 
 
-# Попередня обробка даних
 equipment_df = equipment_df.dropna().drop_duplicates()
 corrections_df = corrections_df.dropna().drop_duplicates()
 personnel_df = personnel_df.dropna().drop_duplicates()
 
-# Виконання перетворень даних (якщо потрібно)
 equipment_df['date'] = pd.to_datetime(equipment_df['date'])
 corrections_df['date'] = pd.to_datetime(corrections_df['date'])
 personnel_df['date'] = pd.to_datetime(personnel_df['date'])
 
-# Аналіз даних
 equipment_stats = equipment_df.describe()
 corrections_stats = corrections_df.describe()
 personnel_stats = personnel_df.describe()
@@ -30,11 +26,9 @@ print(corrections_stats)
 print("\nСтатистика втрат особового складу:")
 print(personnel_stats)
 
-# Групування та сортування
 equipment_grouped = equipment_df.groupby(equipment_df['date'].dt.year).agg({'tanks': 'sum', 'aircraft': 'sum', 'ships': 'sum'})
 equipment_sorted = equipment_df.sort_values(by='tanks', ascending=False)
 
-# Візуалізація даних
 plt.figure(figsize=(10, 5))
 plt.plot(equipment_df['date'], equipment_df['tanks'])
 plt.xlabel('Дата')
